45e199fc06f4a805c00d4631573dd13763033aa7
siliconshecky <mbkavka@siliconshecky.com>
2021-10-29 11:17:05 -0500

    Update random FQND generator.py

--- random FQND generator.py
+++ random FQND generator.py
@@ -14,17 +14,18 @@ print('Enter domain name: ')
 basedomain = input()
 separator = '.'
 
+# loop for multiple iterations
 i = 0
-while i != 10:
+while i != 10: # change this number for how long this will loop for
     i = i+1
-    # generate 3 character subdomain
+    # generate 3 character subdomain Change range in subdomain variable for more characters
     characters = string.ascii_letters + string.digits
     subdomain = ''.join(random.choice(characters) for i in range(3))
     fqdn = subdomain + separator + basedomain
     print(fqdn)
 
 
-    try:
+    try: # try sequence to see if domain exists, and if an exception is thrown it will say that the domain does not exist
         result = dns.resolver.resolve(fqdn, 'A', raise_on_no_answer=False) 
         for ipval in result:
             print('IP', ipval.to_text())
