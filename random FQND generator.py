595dc35a4be620e43f4c1cb85939c60553e5aeb3
siliconshecky <mbkavka@siliconshecky.com>
2021-10-29 10:50:27 -0500

    Add files via upload

---
+++ random FQND generator.py
@@ -0,0 +1,26 @@
+import sys
+import random
+import string
+import dns as dns
+from dns.rcode import NXDOMAIN
+import dns.resolver
+
+# ask for domain name
+print('Enter domain name: ')
+basedomain = input()
+separator = '.'
+
+# generate 3 character subdomain
+characters = string.ascii_letters + string.digits
+subdomain = ''.join(random.choice(characters) for i in range(3))
+fqdn = subdomain + separator + basedomain
+print(fqdn)
+
+
+try:
+    result = dns.resolver.resolve(fqdn, 'A', raise_on_no_answer=False) 
+    for ipval in result:
+        print('IP', ipval.to_text())
+except dns.resolver.NXDOMAIN:
+    print("Domain does not exist")    
+sys.exit("End of Line")
